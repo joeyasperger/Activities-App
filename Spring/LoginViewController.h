e7cd6aba1a155e01d0f7ca3b5f4c9f38d4cc75de
joeyasperger <joeyasperger@gmail.com>
2014-09-09 10:50:12 -0700

    added login screen

---
+++ Spring/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Spring
+//
+//  Created by Joseph Asperger on 9/9/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
