54c81b6ccc703d9d86f4f65042f1dcdc4b35337a
joeyasperger <joeyasperger@gmail.com>
2014-09-08 23:47:13 -0700

    worked on friends

---
+++ Spring/UserProfile.h
@@ -0,0 +1,13 @@
+//
+//  UserProfile.h
+//  Spring
+//
+//  Created by Joseph Asperger on 9/8/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserProfile : NSObject
+
+@end
---
+++ Spring/UserProfile.m
@@ -0,0 +1,13 @@
+//
+//  UserProfile.m
+//  Spring
+//
+//  Created by Joseph Asperger on 9/8/14.
+//
+//
+
+#import "UserProfile.h"
+
+@implementation UserProfile
+
+@end
