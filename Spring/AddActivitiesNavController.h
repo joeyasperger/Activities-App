bb1db287d36099f352fd736f25f6ef5ea9a22ceb
joeyasperger <joeyasperger@gmail.com>
2014-09-16 12:44:23 -0700

    worked on adding activities

---
+++ Spring/AddActivitiesNavController.h
@@ -0,0 +1,13 @@
+//
+//  AddActivitiesNavigationController.h
+//  Spring
+//
+//  Created by Joseph Asperger on 9/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddActivitiesNavigationController : UINavigationController
+
+@end
---
+++ Spring/AddActivitiesNavController.m
@@ -0,0 +1,49 @@
+//
+//  AddActivitiesNavigationController.m
+//  Spring
+//
+//  Created by Joseph Asperger on 9/15/14.
+//
+//
+
+#import "AddActivitiesNavigationController.h"
+
+@interface AddActivitiesNavigationController ()
+
+@end
+
+@implementation AddActivitiesNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
