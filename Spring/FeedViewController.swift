6e1143d360fc3a23a35213299793f410c3e139ff
joeyasperger <joeyasperger@gmail.com>
2014-12-30 15:38:56 -0800

    converted login view controller to swift

---
+++ Spring/FeedViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FeedViewController.swift
+//  Spring
+//
+//  Created by Joseph Asperger on 12/30/14.
+//
+//
+
+import UIKit
+
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Spring/LoginViewController.swift
@@ -0,0 +1,83 @@
+//
+//  LoginViewController.swift
+//  Spring
+//
+//  Created by Joseph Asperger on 12/30/14.
+//
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate, FBLoginViewDelegate {
+   
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet var facebookLoginView: FBLoginView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        errorLabel.text = ""
+        emailField.text = "joeyasperger@gmail.com"
+        facebookLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+    }
+    
+    @IBAction func login(sender: AnyObject) {
+        sendLoginRequest()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func sendLoginRequest() {
+        PFUser.logInWithUsernameInBackground(emailField.text, password: passwordField.text) { (user, error) -> Void in
+            if ((user) != nil){
+                println("success")
+                CurrentUserRelations.downloadRelations()
+                self.performSegueWithIdentifier("LoginSegue", sender: self)
+            }
+            else{
+                if let errorString = error.userInfo?["error"] as? String {
+                    self.errorLabel.text = errorString
+                }
+            }
+        }
+    }
+    
+    func transitionAfterLogin() {
+        performSegueWithIdentifier("LoginSegue", sender: self)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if (textField == passwordField){
+            sendLoginRequest()
+        }
+        if (textField == emailField){
+            passwordField.becomeFirstResponder()
+        }
+        return true
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+    }
+    
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "LoginSegue"){
+            
+        }
+    }
+    
+}
