de57789b92e79fa25573c2220b1a8057802e1034
joeyasperger <joeyasperger@gmail.com>
2015-03-18 01:09:58 -0700

    changed select location to use static marker in center of screen

--- RollOut/SelectEventLocationViewController.m
+++ RollOut/SelectEventLocationViewController.m
@@ -16,7 +16,6 @@
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property double latitude;
 @property double longitude;
-@property GMSMarker *eventLocationMarker;
 
 @end
 
@@ -25,31 +24,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Create a GMSCameraPosition that tells the map to display the
-    // coordinate -33.86,151.20 at zoom level 6.
     self.latitude = 37.43;
     self.longitude = -122.17;
     if (self.passedLocation){
         self.latitude = self.location.latitude;
         self.longitude = self.location.longitude;
     }
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:self.latitude longitude:self.longitude zoom:13];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:self.latitude longitude:self.longitude zoom:17];
     self.mapView = [GMSMapView mapWithFrame:self.view.bounds camera:camera];
     UIEdgeInsets mapInsets = UIEdgeInsetsMake(90, 0, 0, 0);
-    
     self.mapView.padding = mapInsets;
     self.mapView.myLocationEnabled = YES;
     self.mapView.settings.myLocationButton = YES;
     self.mapView.settings.compassButton = YES;
     [self.view insertSubview:self.mapView atIndex:0];
     self.mapView.delegate = self;
-    self.eventLocationMarker = [GMSMarker new];
-    self.eventLocationMarker.icon = [GMSMarker markerImageWithColor:[UIColor greenColor]];
-    self.eventLocationMarker.position = CLLocationCoordinate2DMake(self.latitude , self.longitude);
-    self.eventLocationMarker.map = self.mapView;
-    self.mapView.selectedMarker = self.eventLocationMarker;
-    self.eventLocationMarker.title = @"Event Location";
-    self.eventLocationMarker.draggable = YES;
+    
+    UIImage *markerImage = [GMSMarker markerImageWithColor:[UIColor greenColor]];
+    UIImageView *pinView = [[UIImageView alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2 - markerImage.size.width/2, self.view.frame.size.height/2, markerImage.size.width, markerImage.size.height)];
+    pinView.image = markerImage;
+    [self.view addSubview:pinView];
 }
 
 - (IBAction)done:(id)sender {
@@ -79,30 +73,9 @@
 
 -(BOOL) mapView:(GMSMapView *)mapView didTapMarker:(GMSMarker *)marker{
     [self hideKeyboard];
-    if (marker != self.eventLocationMarker){
-        [self resetMarkerColor:self.eventLocationMarker];
-        // select new marker and set its color to green
-        self.eventLocationMarker = marker;
-        mapView.selectedMarker = marker;
-        mapView.selectedMarker.icon = [GMSMarker markerImageWithColor:[UIColor greenColor]];
-    }
-    mapView.selectedMarker = marker;
-    return YES;
+    return NO;
 }
 
--(void) mapView:(GMSMapView *)mapView didBeginDraggingMarker:(GMSMarker *)marker{
-    [self hideKeyboard];
-    if (marker != self.eventLocationMarker){
-        [self resetMarkerColor:self.eventLocationMarker];
-        // select new marker and set its color to green
-        self.eventLocationMarker = marker;
-        mapView.selectedMarker = marker;
-        mapView.selectedMarker.icon = [GMSMarker markerImageWithColor:[UIColor greenColor]];
-    }
-    marker.title = @"Event Location";
-    marker.snippet = nil;
-    mapView.selectedMarker = marker;
-}
 
 -(void) mapView:(GMSMapView *)mapView willMove:(BOOL)gesture{
     [self hideKeyboard];
@@ -110,21 +83,6 @@
 
 -(void) mapView:(GMSMapView *)mapView didTapAtCoordinate:(CLLocationCoordinate2D)coordinate{
     [self hideKeyboard];
-}
-
--(GMSMarker*) resetMarkerColor:(GMSMarker*)marker{
-    if ([marker.title  isEqual: @"Event Location"]){
-        marker.map = nil;
-        return nil;
-    }
-    GMSMarker *newMarker = [GMSMarker new];
-    newMarker.position = marker.position;
-    newMarker.title = marker.title;
-    newMarker.snippet = marker.snippet;
-    marker.map = nil; //remove old selected marker from map
-    newMarker.map = self.mapView;
-    newMarker.draggable = YES;
-    return newMarker;
 }
 
 -(void) hideKeyboard{
@@ -147,9 +105,8 @@
             NSLog(@"%@", error);
         }
         else{
-            //success!
             //NSLog(@"Google Data: %@", places);
-            BOOL didSetSelected = NO;
+            BOOL didSetCamera = NO;
             [self.mapView clear];
             NSArray* places = [json objectForKey:@"results"];
             for (NSDictionary *place in places){
@@ -163,15 +120,12 @@
                 marker.position = CLLocationCoordinate2DMake(latitude , longitude);
                 marker.title = name;
                 marker.snippet = address;
-                marker.draggable = YES;
                 marker.map = self.mapView;
-                if (!didSetSelected){
-                    self.eventLocationMarker = marker;
+                if (!didSetCamera){
                     self.mapView.selectedMarker = marker;
-                    marker.icon = [GMSMarker markerImageWithColor:[UIColor greenColor]];
-                    didSetSelected = YES;
                     GMSCameraUpdate *update = [GMSCameraUpdate setTarget:CLLocationCoordinate2DMake(latitude, longitude)];
                     [self.mapView animateWithCameraUpdate:update];
+                    didSetCamera = YES;
                 }
             }
         }
@@ -180,10 +134,8 @@
 
 -(void) viewWillDisappear:(BOOL)animated{
     NSString *locationName = nil;
-    if (![self.eventLocationMarker.title isEqualToString:@"Event Location"]){
-        locationName = self.eventLocationMarker.title;
-    }
-    [self.delegate recieveLocation:self.eventLocationMarker.position name:locationName];
+    [self.delegate recieveLocation:self.mapView.camera.target name:locationName];
+
 }
 
 @end
